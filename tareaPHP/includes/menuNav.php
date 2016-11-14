a233116cc9119752a0810e36f6b164a31f5ae414
Mathias <Mathias@DESKTOP-01MNNDE>
2016-11-14 10:26:39 -0300

    logo BCMedia

--- tareaPHP/includes/menuNav.php
+++ tareaPHP/includes/menuNav.php
@@ -12,7 +12,7 @@ require "loginModal.php";
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span> 
             </button>
-            <a class="navbar-brand" href="/tareaPHP/index.php">Inicio</a>
+            <a class="navbar-brand" href="/tareaPHP/index.php"><img src="/tareaPhp/img/sitio/BCMediaPng2.png" height="25px"></a>
         </div>
         <div class="collapse navbar-collapse" id="opciones">
         
